3497f820a9f9e3835eda7922557482a45d783672
Dolapo Alli <dolapoalli@Dolapos-MBP.attlocal.net>
2022-07-27 16:21:07 -0400

    new home

--- src/pages/Home .tsx
+++ src/pages/Home .tsx
@@ -8,22 +8,24 @@ const Home = () => {
 
 
   return (
-    <Row>
-      <HeaderCarousel />
-      <Row style={{ display: "flex", flexDirection: "column" }}>
-        <Row style={{marginTop: "5px" }}>
-          <Col style={{ width: "49.8%", height: "70vh", backgroundColor: "red" }}>Big Lef</Col>
-          <Col style={{ width: "49.8%", height: "70vh", backgroundColor: "blue", display: "flex", flexDirection: "column"}}>
-            <Row style={{height:"49.8%%", backgroundColor:"yellow"}}>Top right</Row>
-            <Row>
-              <Col>Bottom right left</Col>
-              <Col>Bottom right right</Col>
-            </Row>
-          </Col>
-        </Row>
-        <div style={{ backgroundColor: "yellow", width: "100%", height: "12vh", marginTop:"5px" }}></div>
-      </Row>
-    </Row>
+    // <Row>
+    //   <HeaderCarousel />
+    //   <Row style={{ display: "flex", flexDirection: "column" }}>
+    //     <Row style={{marginTop: "5px" }}>
+    //       <Col style={{ width: "49.8%", height: "70vh", backgroundColor: "red" }}>Big Lef</Col>
+    //       <Col style={{ width: "49.8%", height: "70vh", backgroundColor: "blue", display: "flex", flexDirection: "column"}}>
+    //         <Row style={{height:"49.8%%", backgroundColor:"yellow"}}>Top right</Row>
+    //         <Row>
+    //           <Col>Bottom right left</Col>
+    //           <Col>Bottom right right</Col>
+    //         </Row>
+    //       </Col>
+    //     </Row>
+    //     <div style={{ backgroundColor: "yellow", width: "100%", height: "12vh", marginTop:"5px" }}></div>
+    //   </Row>
+    // </Row>
+
+    <h1>Home Page coming Soon</h1>
   )
 }
 
